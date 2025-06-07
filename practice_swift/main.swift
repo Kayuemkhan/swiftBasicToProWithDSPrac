04eb0b2496b9cf0c0ffa53ebf7307981e319094a
Kayuemkhan <abdulkayuem007@gmail.com>
2025-06-08 02:44:28 +0600

    basics added ( Variables, Constant, Control Statement , Loops , Enum, Switch Case )

--- practice_swift/main.swift
+++ practice_swift/main.swift
@@ -8,24 +8,139 @@
 import Foundation
 
 
-print("Switch Case")
+ //Variables
 
+//var str = "abdul"
+//print(str)
+//
+//str = "Titenium"
+//
+//print(str)
+//
+//
+//var age = 38
+//var population = 8_00_00_000
+//print(population)
+//
+//
+//// Multiline Strings
+//
+//var str1 = """
+//This is a 
+//example of 
+//multiple line of String
+//"""
+//print(str1)
+//
+//// String into String Aka String interpolation
+//
+//var score =  85
+//var StringInterpolation = "Your Score was \(score)"
+//
+//print(StringInterpolation)
+//
+//// Constant
+//
+//let constant = 13
+//print(constant)
+//
+//
+//
+//
+//// Array
+//
+//let arrary = ["Name 1", "Name 2","Name 3","Name 4"]
+//
+//print(arrary)
+//print(arrary[0])
+//
+//
+//
+// Enum Classes
 
-var day = Int(readLine()!)!
-
-switch day{
-case ...1:
-    print("Monday")
-case ...2:
-    print("Tuesday")
-case ...3:
-    print("Wednesday")
-case ...4:
-    print("Thursday")
-case ...5:
-    print("Friday")
-case ...6:
-    print("Saturday")
-default:
-    print("Sunday")
+enum Result {
+    case success
+    case failure
 }
+
+let result = Result.success
+print(result)
+
+enum Planet: Int {
+    case mercury
+    case venus
+    case earth
+    case mars
+}
+let earth = Planet(rawValue: 2)
+print(earth)
+
+
+// control statement
+
+let score = 85
+
+if score >= 90 {
+    print("A grade")
+} else if score >= 80 {
+    print("B grade")
+} else {
+    print("C grade")
+}
+
+
+
+
+
+//print("Switch Case")
+//
+//
+//var day = Int(readLine()!)!
+//
+//switch day{
+//case ...1:
+//    print("Monday")
+//case ...2:
+//    print("Tuesday")
+//case ...3:
+//    print("Wednesday")
+//case ...4:
+//    print("Thursday")
+//case ...5:
+//    print("Friday")
+//case ...6:
+//    print("Saturday")
+//default:
+//    print("Sunday")
+//}
+
+//✅ Supports value ranges:
+
+let mark = 75
+
+switch mark {
+case 90...100:
+    print("Excellent")
+case 70..<90:
+    print("Good")
+default:
+    print("Keep trying")
+}
+
+// for loop
+
+for i in 1...10 {
+    print(i)
+}
+
+// while loop
+
+var count = 0
+while count < 3 {
+    print("Count is \(count)")
+    count += 1
+}
+
+repeat {
+    print("Run at least once")
+} while count < 3
